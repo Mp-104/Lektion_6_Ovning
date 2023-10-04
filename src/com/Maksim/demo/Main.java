2a5328e009f92626976b34316afa573ebb770c49
Maksim <Mp-104@hotmail.com>
2023-10-04 17:21:07 +0200

    uppgift 1

--- src/com/Maksim/demo/Main.java
+++ src/com/Maksim/demo/Main.java
@@ -1,2 +1,13 @@
-package com.Maksim.demo;public class Main {
+package com.Maksim.demo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+
+        }
+
+
+    }
 }
